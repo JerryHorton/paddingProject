3b1d43fad9b25d32165a3b5db93612f66d96ff87
songxiangyu <2440021574@qq.com>
2023-06-29 16:04:50 +0800

    无

--- src/main/java/com/example/padding/controller/SchoolController.java
+++ src/main/java/com/example/padding/controller/SchoolController.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.padding.common.R;
 import com.example.padding.entity.School;
-import com.example.padding.entity.User;
 import com.example.padding.service.SchoolService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
